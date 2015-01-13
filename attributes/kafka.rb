9b7288f1996827b4e10cc3180056f4101acaa887
Mike Heffner <mikeh@fesnel.com>
2015-01-12 23:06:45 -0500

    Update mirror site.

--- attributes/kafka.rb
+++ attributes/kafka.rb
@@ -1,5 +1,5 @@
 default[:kafka][:version] = '0.7.1-incubating'
-default[:kafka][:mirror] = 'http://mirrors.ibiblio.org/apache/incubator/kafka/kafka-{version}/kafka-{version}-src.tgz'
+default[:kafka][:mirror] = 'http://archive.apache.org/dist/kafka/old_releases/kafka-{version}/kafka-{version}-src.tgz'
 default[:kafka][:sha256sum] = "99ec0641de3646229c1a598023274b22d4229f21cd7f95d7043494a249bdc3c3"
 
 # Where to store the commitlogs. NOTE: This should be changed
